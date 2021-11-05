61ab33b6b33a6e69de40cab215d11d5998fd3e3b
Administrator <294350757@qq.com>
2021-11-05 21:45:56 +0800

    resault

---
+++ MxOnlinee/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for MxOnlinee project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MxOnlinee.settings')
+
+application = get_asgi_application()
